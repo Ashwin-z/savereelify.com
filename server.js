62067e2bfa17bc8b769b663e43fc298c4d208521
Girach-ak <akgirach5@gmail.com>
2025-02-13 17:13:19 -0700

    gitter

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware setup
-app.use(cors());
+app.use(cors({ origin: 'https://savereelify.com' }));
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
